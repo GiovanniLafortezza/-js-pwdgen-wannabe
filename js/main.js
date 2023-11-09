677bd9ff155016bfd3392787a8fbe05a4d479543
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-09 13:46:10 +0100

    bonus start

--- js/main.js
+++ js/main.js
@@ -4,44 +4,18 @@
 let name = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
 let favouriteColor = prompt("Inserisci il tuo colore preferito");
+let primoNumero = prompt("Inserisci il primo numero");
+let secondoNumero =prompt("Inserisci il secondo numero");
 
-
-
+let divisioneNumeri = primoNumero / secondoNumero; 
 let businessCard=
 `
 > Informazioni personali:
-Nome e Cognome: ${name} ${surname} ${favouriteColor} 21
+Nome e Cognome: ${name}${surname}${favouriteColor}${divisioneNumeri}
+
 `;
 
 console.log(businessCard);
 
-// Qui posso poi compilare l'eventuale pagina HTML prodisposta.
 
 
-/* OUTPUT PREVISTO DAL MIO SCRIPT
--------------- Il tuo bigliettino da visita --------------
-> Informazioni personali:
-Nome e Cognome: Pinco Pallino
-Eta': 20
-
-> Informazioni di contatto:
-Email: pincopallino@gmail.com
-
-> Informazioni sul lavoro:
-Professione: Studente
--------------------------------------------------------------
-*/
-
-/* Variante funzionante ma meno elegante
-console.log("-------------- Il tuo bigliettino da visita --------------");
-console.log("> Informazioni personali:");
-console.log("Nome e Cognome: " + name + " " + surname);
-console.log("Eta': " + age);
-console.log("");
-console.log("> Informazioni di contatto:");
-console.log("Email: " + email);
-console.log("");
-console.log(">Informazioni di lavoro:");
-console.log("Professione: " + profession);
-console.log("-------------------------------------------------------------");
-*/
